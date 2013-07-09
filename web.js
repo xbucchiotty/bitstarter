5869b687e48277264b31f46b008e9b60390c7e3d
Xavier Bucchiotty <xbucchiotty@xebia.fr>
2013-07-09 07:25:35 +0200

    index.html

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
-var express = require('express');
+var express = require('express'), fs =require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    fs.readFile('./index.html', function (err, html) {
+       if (err) {
+           throw err; 
+       }       
+       response.writeHeader(200, {"Content-Type": "text/html"});  
+       response.write(html);  
+       response.end();  
+    });
 });
 
 var port = process.env.PORT || 5000;
